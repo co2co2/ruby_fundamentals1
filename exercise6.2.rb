3a5f82d5a65b95c64d5e08c7a7eb51129a912ccd
co2co2 <coco8287@hotmail.com>
2017-10-30 19:02:31 -0400

    exercise 6.2

---
+++ exercise6.2.rb
@@ -0,0 +1,19 @@
+energy = 100
+until energy <= 0
+puts "what Would you like to do?"
+answer= gets.chomp
+if answer =="walk"
+  energy += 24
+  puts "energy level is #{energy}"
+elsif answer =="run"
+  energy -= 25
+  puts  "energy level is #{energy}"
+elsif answer == "eat"
+  energy += 30
+  puts "energy level is #{energy}"
+elsif answer == "rest"
+  energy -= 20
+  puts "energy level is #{energy}"
+
+end
+end
