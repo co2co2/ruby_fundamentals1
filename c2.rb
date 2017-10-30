61b966342ce92c890c467eabe4699d376aefb9bc
co2co2 <coco8287@hotmail.com>
2017-10-30 16:28:18 -0400

    3rd challenge

---
+++ c2.rb
@@ -0,0 +1,5 @@
+puts "How old are you?"
+age = gets.chomp.to_i
+if age > 105
+  puts "I'm not sure i believe you."
+end
---
+++ c3.rb
@@ -0,0 +1,6 @@
+my_name ="coco"
+puts "What's your name?"
+your_name = gets.chomp
+if your_name == "coco"
+  puts "we have the same name!"
+end
