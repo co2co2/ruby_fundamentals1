679f6cfed20743a2b9a487d6b3e5be080d2bbeba
co2co2 <coco8287@hotmail.com>
2017-10-30 14:57:07 -0400

    ex 2 first commit

---
+++ exercise2.rb
@@ -0,0 +1,8 @@
+# 15% tip is standard while 20%is a good tip
+puts 55 * 0.2
+#TypeError fixed
+puts "good morning" + 87.to_s
+#string interpolation shows the result.
+puts " #{45628 * 7839}is a big number"
+#true
+puts (10 < 20 && 30 < 20) || !(10 == 11)
