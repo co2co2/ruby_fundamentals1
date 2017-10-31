ae3534016c5cebb25d18edee1d5fd32b159eb581
co2co2 <coco8287@hotmail.com>
2017-10-30 23:33:47 -0400

    excise 6 modification

--- exercise6.2.rb
+++ exercise6.2.rb
@@ -1,7 +1,7 @@
-energy = 100
+energy =  100
 until energy <= 0
-puts "what Would you like to do?"
-answer= gets.chomp
+  puts "what Would you like to do?"
+  answer= gets.chomp
 if answer =="walk"
   energy += 24
   puts "energy level is #{energy}"
@@ -12,8 +12,21 @@ elsif answer == "eat"
   energy += 30
   puts "energy level is #{energy}"
 elsif answer == "rest"
-  energy -= 20
+  energy += 20
   puts "energy level is #{energy}"
+end
 
+while energy <=0
+  puts "no more runing.take a rest,eat or walk?"
+  answer= gets.chomp
+if answer =="walk"
+    energy += 24
+elsif answer == "eat"
+  energy += 30
+  puts "energy level is #{energy}"
+elsif answer == "rest"
+  energy += 20
+  puts "energy level is #{energy}"
+end
 end
 end
